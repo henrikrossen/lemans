cd930b5ce2b9ab829e9bcca3b41b4906f897a7e4
Jesper Blad Jensen <deldy@deldysoft.dk>
2014-06-14 19:56:34 +0200

    basic widget

---
+++ public/javascripts/position-map.js
@@ -0,0 +1,16 @@
+window.widget = window.widget || {};
+window.widget["positionMap"] = (function() {
+
+	var init = function() {
+
+	};
+
+	var bind = function(data) {
+
+	}
+
+	return {
+		init: init,
+		bind: bind
+	}
+})();
