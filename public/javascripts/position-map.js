b50df5f4a4f4ee2babe67f116c9ae9b4ef9bac95
Jesper Blad Jensen <deldy@deldysoft.dk>
2014-06-14 23:16:53 +0200

    fix for long pit

--- public/javascripts/position-map.js
+++ public/javascripts/position-map.js
@@ -24,7 +24,7 @@ window.widget["positionMap"] = (function() {
 
 
 	var bind = function(data) {
-		var filtered = _.filter(data.cars, function(x) { return x.driverStatus == 2 || x.driverStatus == 4; });
+		var filtered = _.filter(data.cars, function(x) { return x.driverStatus == 2 || x.driverStatus == 4 || x.driverStatus == 3 });
 
 
 		var a = _.groupBy(filtered, function(x) { return x.laps});
