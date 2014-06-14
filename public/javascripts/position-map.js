015b97aa78436cad4a994c120b51abf628f24187
Jesper Blad Jensen <deldy@deldysoft.dk>
2014-06-14 21:09:43 +0200

    Colors on cars

--- public/javascripts/position-map.js
+++ public/javascripts/position-map.js
@@ -8,23 +8,26 @@ window.widget["positionMap"] = (function() {
 
 	var bind = function(data) {
 		var filtered = _.filter(data.cars, function(x) { return x.driverStatus == 2 || x.driverStatus == 4; });
-		var mapped = $.map(filtered, function(val) {
-			return {
-				laps: val.laps,
-				number: val.number
-			}
-		});
 
 
-		var a = _.groupBy(mapped, function(x) { return x.laps});
+		var a = _.groupBy(filtered, function(x) { return x.laps});
 		var html = "";
 		_.each(a, function(lap) {
 			var lapHtml = "<div class='lap'><header>%lap%</header>";
 			html += lapHtml.replace("%lap%", lap[0].laps);
 
 			_.each(lap, function(car) {
-				var carHtml = "<div class='driver'>%car%</div>";
-				html += carHtml.replace("%car%", car.number);
+				var carHtml = "<div class='driver %class%'>%car%</div>";
+				var category = "";
+
+				if(car.category != null) {
+					category = car.category;
+				}
+
+				carHtml = carHtml.replace("%car%", car.number);
+				carHtml = carHtml.replace("%class%", category.replace(/\s/g,'-').toLowerCase());
+				
+				html += carHtml;
 			})
 
 			html += "</div>";
