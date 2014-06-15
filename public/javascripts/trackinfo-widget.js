f60e3e19d6a5ef86de42338106f8db346232197c
Jesper Blad Jensen <deldy@deldysoft.dk>
2014-06-15 12:48:34 +0200

    stuff

--- public/javascripts/trackinfo-widget.js
+++ public/javascripts/trackinfo-widget.js
@@ -2,10 +2,20 @@ window.widget = window.widget || {};
 window.widget["trackinfo"] = (function() {
 
 	var template;
+	var elapsedTimeInSeconds = 0;
+	var remainingTimeInSeconds = 0;
 
 	var init = function() {
 		var source = $("#track-template").html(); 
 		template = Handlebars.compile(source); 
+
+		setInterval(function() {	
+			elapsedTimeInSeconds = elapsedTimeInSeconds + 1;
+			$("#elapsedTime").html(secondsToFormattedTime(elapsedTimeInSeconds));
+
+			remainingTimeInSeconds = remainingTimeInSeconds - 1;
+			$("#remainingTime").html(secondsToFormattedTime(remainingTimeInSeconds));
+		}, 1000);
 	};
 
 	var bind = function(data) {
@@ -18,6 +28,9 @@ window.widget["trackinfo"] = (function() {
 		  return qualify ? "Yes" : "No";
 		});
 
+		elapsedTimeInSeconds = data.elapsedTimeInSeconds;
+		remainingTimeInSeconds = data.remainingTimeInSeconds;
+			
 		$('#trackInfo').html(template(data));
 	}
 
@@ -26,3 +39,16 @@ window.widget["trackinfo"] = (function() {
 		bind: bind
 	}
 })();
+
+ function secondsToFormattedTime(val) {
+    var sec_num = parseInt(val, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    var time    = hours + ' : ' + minutes + ' : ' + seconds;
+    return time;
+}
